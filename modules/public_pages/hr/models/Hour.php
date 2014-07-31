bedc1978917c29de1b21eb98ad94e4654b211ad1
Martyn Shiner <mshiner@uzerp.com>
2014-07-31 09:50:22 +0100

    Fixed Bug to limit person dropdown when allocating task hours When entering task hours person dropdown now only shows people from the system company Note - this means that 'non employees' can still book hours IF they are in the system company as people.

--- modules/public_pages/hr/models/Hour.php
+++ modules/public_pages/hr/models/Hour.php
@@ -35,7 +35,9 @@ class Hour extends DataObject
 		$this->orderdir='DESC';
 		
 // Define relationships
- 		$this->belongsTo('Person', 'person_id', 'person', null, "surname || ', ' || firstname");
+		$cc=new ConstraintChain();
+		$cc->add(new Constraint('company_id', '=', EGS_COMPANY_ID));
+ 		$this->belongsTo('Person', 'person_id', 'person',  $cc, "surname || ', ' || firstname");
 		$this->belongsTo('Opportunity', 'opportunity_id', 'opportunity'); 
 		$this->belongsTo('Project', 'project_id', 'project');
  		$this->belongsTo('Task', 'task_id', 'task');
