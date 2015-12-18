353f2a7de6b06e08c8d498a05cb117034c6dfe8e
Steve Blamey <blameys@blueloop.net>
2015-12-18 11:59:20 +0000

    Code cleanup

--- modules/public_pages/crm/models/Activity.php
+++ modules/public_pages/crm/models/Activity.php
@@ -1,43 +1,44 @@
 <?php
 
-/** 
- *	(c) 2000-2012 uzERP LLP (support#uzerp.com). All rights reserved. 
- * 
- *	Released under GPLv3 license; see LICENSE. 
+/**
+ *  CRM Activity Model
+ *
+ *  @package crm
+ *  @author uzERP LLP and Steve Blamey <blameys@blueloop.net>
+ *  @license GPLv3 or later
+ *  @copyright (c) 2000-2015 uzERP LLP (support#uzerp.com). All rights reserved.
  **/
-
 class Activity extends DataObject
 {
-	
-	protected $version = '$Revision: 1.7 $';
-	
-	protected $defaultDisplayFields = array('name'			=> 'Name'
-										   ,'opportunity'	=> 'Attached to'
-										   ,'company'		=> 'Company'
-										   ,'person'		=> 'Person'
-										   ,'startdate'		=> 'Start Date'
-										   ,'enddate'		=> 'End Date'
-										   );
-	
-	function __construct($tablename = 'activities')
-	{
-		parent::__construct($tablename);
-		
-		$this->idField	= 'id';
-		$this->orderby	= 'startdate';
-		$this->orderdir	= 'desc';
-		
-		$this->belongsTo('Activitytype', 'type_id', 'type');
-		$this->belongsTo('User', 'owner', 'activity_owner');
-		$this->belongsTo('User','assigned','activity_assigned');
-		$this->belongsTo('User', 'alteredby', 'activity_alteredby');
-		$this->belongsTo('Opportunity', 'opportunity_id', 'opportunity');
-		$this->belongsTo('Campaign', 'campaign_id', 'campaign');
-		$this->belongsTo('Company', 'company_id', 'company');
-		$this->belongsTo('Person', 'person_id', 'person', null, 'surname || \', \' || firstname');
 
-	}
+    protected $version = '$Revision: 1.7 $';
 
+    protected $defaultDisplayFields = array(
+        'name' => 'Name',
+        'opportunity' => 'Attached to',
+        'company' => 'Company',
+        'person' => 'Person',
+        'startdate' => 'Start Date',
+        'enddate' => 'End Date'
+    );
+
+    function __construct($tablename = 'activities')
+    {
+        parent::__construct($tablename);
+
+        $this->idField = 'id';
+        $this->orderby = 'startdate';
+        $this->orderdir = 'desc';
+
+        $this->belongsTo('Activitytype', 'type_id', 'type');
+        $this->belongsTo('User', 'owner', 'activity_owner');
+        $this->belongsTo('User', 'assigned', 'activity_assigned');
+        $this->belongsTo('User', 'alteredby', 'activity_alteredby');
+        $this->belongsTo('Opportunity', 'opportunity_id', 'opportunity');
+        $this->belongsTo('Campaign', 'campaign_id', 'campaign');
+        $this->belongsTo('Company', 'company_id', 'company');
+        $this->belongsTo('Person', 'person_id', 'person', null, 'surname || \', \' || firstname');
+    }
 }
 
 // End of Activity
