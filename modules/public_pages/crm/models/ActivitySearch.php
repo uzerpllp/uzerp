655a0d55e0f59ac0bcc41cb5b1109577f5573ed4
Steve Blamey <blameys@blueloop.net>
2016-01-06 15:16:48 +0000

    CRM Activity improvements
    Allow search by type, start-date/end-date range
    Remove timeframe search

--- modules/public_pages/crm/models/ActivitySearch.php
+++ modules/public_pages/crm/models/ActivitySearch.php
@@ -19,10 +19,21 @@ class ActivitySearch extends BaseSearch
         $search->addSearchField('completed', 'show_completed', 'show', 'NULL');
         $search->setOffValue('completed', 'NULL');
         $search->addSearchField('name', 'name_contains', 'contains');
+
+        // Search by activity type
+        $search->addSearchField('type_id', 'type', 'select');
+        $activity_type = DataObjectFactory::Factory('Activitytype');
+        $options = array('0'=>'All');
+        $activity_types = $activity_type->getAll(null, false, true, '', '');
+        $options +=$activity_types;
+        $search->setOptions('type_id', $options);
+
         $search->addSearchField('assigned', 'assigned_to_me', 'hide', false);
-        $search->addSearchField('enddate', 'timeframe', 'timeframe', '');
+        // $search->addSearchField('enddate', 'timeframe', 'timeframe', '');
         $search->setOnValue('assigned', EGS_USERNAME);
 
+        $search->addSearchField('startdate', 'start date between', 'between', '', 'advanced');
+        $search->addSearchField('enddate', 'end date between', 'between', '', 'advanced');
         $search->addSearchField('company', 'company_name', 'begins', '', 'advanced');
         $search->addSearchField('person', 'person', 'contains', '', 'advanced');
 
