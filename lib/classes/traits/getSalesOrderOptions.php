faf44e6678fc35f318eae96d6e7a7052ffeaf2bd
Steve Blamey <blameys@blueloop.net>
2016-04-27 13:49:13 +0100

    Order sales order list by sales order number closes #117

--- lib/classes/traits/getSalesOrderOptions.php
+++ lib/classes/traits/getSalesOrderOptions.php
@@ -27,29 +27,30 @@ trait getSalesOrderOptions
     private function getSalesOrders($_order_id = '')
     {
         $sorder = DataObjectFactory::Factory('SOrder');
-        
+
         $sorder->identifierField = array(
             'order_number',
             'customer',
             'person'
         );
-        
+        $sorder->orderby = 'order_number';
+
         $cc = new ConstraintChain();
         $cc1 = new ConstraintChain();
-        
+
         $cc1->add(new Constraint('type', '=', $sorder->sales_order()));
         $cc1->add(new Constraint('status', 'in', "('" . $sorder->newStatus() . "','" . $sorder->openStatus() . "')"));
-        
+
         // TODO: Check that orderlines for each order satisfy the conditions in getOrderLines above
-        
+
         $cc->add($cc1);
-        
+
         if (! empty($_order_id)) {
             $cc2 = new ConstraintChain();
             $cc2->add(new Constraint('id', '=', $_order_id));
             $cc->add($cc2, 'OR');
         }
-        
+
         return $sorder->getAll($cc, true, true);
     }
 }
