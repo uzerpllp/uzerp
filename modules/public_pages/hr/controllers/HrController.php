f2d920379a5885cee91c5aa8415499bc7855a144
Steve Blamey <blameys@blueloop.net>
2017-01-21 11:59:38 +0000

    Add option to return the employee object instead of the id

--- modules/public_pages/hr/controllers/HrController.php
+++ modules/public_pages/hr/controllers/HrController.php
@@ -1,36 +1,37 @@
 <?php
 
-/** 
- *	(c) 2000-2012 uzERP LLP (support#uzerp.com). All rights reserved. 
- * 
- *	Released under GPLv3 license; see LICENSE. 
+/**
+ *	(c) 2000-2012 uzERP LLP (support#uzerp.com). All rights reserved.
+ *
+ *	Released under GPLv3 license; see LICENSE.
  **/
 
 class HrController extends printController
 {
 
 	protected $version='$Revision: 1.1 $';
-	
+
 	/*
 	 * Get the employee id for the current user
 	 */
-	protected function get_employee_id()
+	protected function get_employee_id($object=false)
 	{
 
 		$user = getCurrentUser();
-		
+
 		if ($user && !is_null($user->person_id))
 		{
 			$employee = DataObjectFactory::Factory('Employee');
-			
+
 			$employee->loadBy('person_id', $user->person_id);
-			
-			if ($employee->isLoaded())
+
+			if ($employee->isLoaded() && $object)
 			{
-				return $employee->id;
+			    return $employee;
 			}
+			return $employee->id;
 		}
-		
+
 		// User is not an employee
 		return '';
 	}
