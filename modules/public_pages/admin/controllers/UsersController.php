1c43ff784b4cb1dbc68bc834de60f0e2a5f6ac04
Steve Blamey <blameys@blueloop.net>
2017-03-21 16:41:15 +0000

    Refactor password reset to require a minimum of 10 characters
    Use RandomLib (https://github.com/ircmaxell/RandomLib) to
    generate random password strings.
    
    Add the password strength meter and password count to the
    password entry field. Extends the height of the user selector.

--- modules/public_pages/admin/controllers/UsersController.php
+++ modules/public_pages/admin/controllers/UsersController.php
@@ -1,100 +1,111 @@
 <?php
-
-/** 
- *	(c) 2000-2012 uzERP LLP (support#uzerp.com). All rights reserved. 
- * 
- *	Released under GPLv3 license; see LICENSE. 
- **/
+/**
+ *	uzERP Users Controller
+ *
+ *	@author uzERP LLP and Steve Blamey <blameys@blueloop.net>
+ *	@license GPLv3 or later
+ *	@copyright (c) 2000-2017 uzERP LLP (support#uzerp.com). All rights reserved.
+ *
+ *	uzERP is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	any later version.
+ */
 
 class UsersController extends printController {
 
 	protected $version = '$Revision: 1.27 $';
-	
+
 	public function __construct($module = NULL, $action = NULL)
 	{
-		
+
 		parent::__construct($module, $action);
-		
+
 		$this->_templateobject = DataObjectFactory::Factory('User');
 		$this->uses($this->_templateobject);
-		
+
 	}
-	
+
 	public function reset_passwords()
 	{
-		
+	    $flash = Flash::Instance();
+	    $errors = array();
+
 		if (!isset($this->_data['users']))
 		{
 			$user	= DataObjectFactory::Factory('User');
 			$cc		= new ConstraintChain();
-			
+
 			$cc->add(new Constraint('usercompanyid', '=', EGS_COMPANY_ID));
-			
+
 			$results = $user->getAll($cc);
 			$this->view->set('users', $results);
-			
+
 		}
 		else
 		{
-			
+
 			foreach ($this->_data['users'] as $username)
 			{
-				
+
 				$user = DataObjectFactory::Factory('User');
 				$user->load($username);
-				
+
 				$password = '';
-				
+
 				if (isset($this->_data['password']))
 				{
 					$password = $this->_data['password'];
 				}
-				
-				$password = $user->setPassword($password);
-				
+
+				$password = $user->setPassword($password, $errors);
+				if (count($errors) >0) {
+				    $flash->addErrors($errors);
+				}
+
 				$message = "Your password for ".SERVER_ROOT." has been reset\n" .
 						"Your username is {$user->username}\n" .
 						"Your password is {$password}\n" .
 						"Thank you";
-				
+
 				$subject = 'Password reset';
-				
+
 				$person = DataObjectFactory::Factory('Person');
 				$person->load($user->person_id);
-				
+
 				$to			= $person->email->contactmethod;
 				$headers	= 'From: '.get_config('ADMIN_EMAIL')."\r\n".'X-Mailer: PHP/' . phpversion();
-				
+
 				if ($to <> '')
 				{
 					mail($to, $subject, $message, $headers);
 				}
-				
+
 			}
 
-			sendBack();	
-			
+			sendBack();
+
 		}
-		
+
 	}
 
 	public function index()
 	{
 		$flash = Flash::Instance();
-		
+
 		$errors = array();
-		
+
 		$s_data = array();
-		
+
 // Set context from calling module
 		$this->setSearch('AdminSearch', 'Users', $s_data);
-		
+
 		$this->view->set('clickaction', 'view');
-		
+
 		parent::index(new UserCollection($this->_templateobject));
-		
+
 		$sidebar = new SidebarController($this->view);
-		
+
 		$sidebar->addList(
 			'Actions',
 			array(
@@ -124,20 +135,20 @@ class UsersController extends printController {
 				)
 			)
 		);
-		
+
 		$this->view->register('sidebar', $sidebar);
 		$this->view->set('sidebar', $sidebar);
-		
+
 	}
 
 	public function save()
 	{
-		
+
 		if (!$this->CheckParams($this->modeltype))
 		{
 			sendBack();
 		}
-		
+
 		$flash		= Flash::Instance();
 		$errors 	= array();
 		$user		= $this->_uses[$this->modeltype];
@@ -145,29 +156,29 @@ class UsersController extends printController {
 		$username	= $user_data['username'];
 		$user		= $user->load($username);
 		$new_user	= TRUE;
-		
+
 		if ($user !== FALSE)
 		{
-			
+
 			$new_user = FALSE;
-			
+
 			if ($user_data['password'] == $user->password)
 			{
 				unset($this->_data[$this->modeltype]['password']);
 			}
-			
+
 		}
-		
+
 		if (empty($user_data['lastcompanylogin']))
 		{
 			$this->_data[$this->modeltype]['lastcompanylogin'] = EGS_COMPANY_ID;
 		}
-		
+
 		if (isset($user_data['debug_options']))
 		{
-			
+
 			$debug = DebugOption::getUserOption($username);
-			
+
 			if (isset($user_data['debug_enabled']))
 			{
 				$this->_data['DebugOption']['username']		= $username;
@@ -176,24 +187,24 @@ class UsersController extends printController {
 			}
 			else
 			{
-				
-				if ($debug->isLoaded()) 
+
+				if ($debug->isLoaded())
 				{
 					$debug->delete();
 					unset($this->_data['DebugOption']);
 				}
-				
+
 			}
-			
+
 		}
 		else
 		{
 			unset($this->_data['DebugOption']);
 		}
-		
+
 		if (!parent::save($this->modeltype))
 		{
-			
+
 			if ($new_user)
 			{
 				$this->refresh();
@@ -203,44 +214,44 @@ class UsersController extends printController {
 				$this->_data['username'] = $this->_data[$this->modeltype]['username'];
 				$this->edit();
 			}
-			
+
 			return FALSE;
-			
+
 		}
-		
+
 		if ($new_user)
 		{
-			
+
 			$uca = Usercompanyaccess::Factory(
 				array(
 					'username'		=> $username,
 					'company_id'	=> EGS_COMPANY_ID,
 					'enabled'		=> TRUE
-				), 
-				$errors, 
+				),
+				$errors,
 				'Usercompanyaccess'
 			);
-			
+
 			if ($uca)
 			{
-				
+
 				if (!$uca->save())
 				{
 					$errors[] = 'Failed to save user company access';
 				}
-				
+
 			}
-			
+
 			if (!$uca||count($errors))
 			{
 				$flash->addErrors($errors);
 				sendTo($this->name, 'index', $this->_modules);
-				
+
 			}
-			
+
 			$user		= $this->saved_model;
 			$password	= $user->setPassword($user_data['password']);
-			
+
 			if (!is_null($user->person_id))
 			{
 				$person = DataObjectFactory::Factory('Person');
@@ -251,104 +262,104 @@ class UsersController extends printController {
 			{
 				$to = '';
 			}
-			
+
 			if (empty($to))
 			{
 				$to = $user->email;
 			}
-			
+
 			if ($to <> '')
 			{
-				
+
 				$message = "You have been created an account for ".SERVER_ROOT."\n" .
 						"Your username is {$user->username}\n" .
 						"Your password is {$password}\n" .
 						"Thank you";
 				$subject = 'New Account';
 				$headers = 'From: '.get_config('ADMIN_EMAIL')."\r\n".'X-Mailer: PHP/' . phpversion();
-				
+
 				mail($to, $subject, $message, $headers);
-				
+
 			}
-			
+
 		}
-		
+
 		if (isset( $this->_data[$this->modeltype]['roles']) && is_array( $this->_data[$this->modeltype]['roles']))
 		{
 			$roles = $this->_data[$this->modeltype]['roles'];
 			User::setRoles($user, $roles, $errors);
 			$flash->addErrors($errors);
 		}
-		
+
 		sendTo($this->name, 'index', $this->_modules);
 
 	}
 
 	public function saveroles()
 	{
-		
+
 		$username	= $this->_data['username'];
 		$role		= $this->_data['roles'];
-		
+
 		User::setRoles($username, $roles);
-	
+
 		sendTo($this->name, 'index', $this->_modules);
-		
+
 	}
 
 	public function _new()
 	{
-		
+
 		parent::_new();
-		
+
 		$roles = DataObjectFactory::Factory('Role');
 		$this->view->set('roles', $roles->getAll());
-		
+
 		$sc = DataObjectFactory::Factory('Systemcompany');
-		
+
 		$companies = new SystemcompanyCollection($sc);
 		$this->view->set('companies', $companies->getCompanies());
 
 		$sc->load(EGS_COMPANY_ID);
-		
+
 		$current_people = $sc->getCurrentPeople();
 		$assigned_users = $this->_uses[$this->modeltype]->getAssignedPeople();
-		
+
 		$this->view->set('people', array_diff_key($current_people, $assigned_users));
-		
+
 		$debug = DataObjectFactory::Factory('DebugOption');
 		$this->view->set('debug_options', $debug->getEnumOptions('options'));
-		
+
 	}
 
 	public function edit()
 	{
-		
+
 		$id = $this->_data['username'];
-		
+
 		$this->_uses[$this->modeltype]->load($id);
 		$user = $this->_uses[$this->modeltype];
 
 		$this->view->set('current', $user->roles->getAssoc());
-		
+
 		$companies = new UsercompanyaccessCollection(DataObjectFactory::Factory('Usercompanyaccess'));
-		
+
 		$sh = new SearchHandler($companies, FALSE, FALSE);
 		$sh->addConstraint(new Constraint('username','=', $id));
 		$sh->setFields(array('id', 'usercompanyid'));
 		$companies->load($sh);
-		
+
 		$this->view->set('selected_companies', $companies->getAssoc());
 		$this->view->set('username', $id);
 		$this->view->set('edit', TRUE);
-		
+
 		$this->_new();
 		$this->setTemplateName('new');
-		
+
 		$debug = DebugOption::getUserOption($id);
 		$this->view->set('debug_id', $debug->id);
 		$this->view->set('selected_options', $debug->getOptions());
-		
+
 	}
 
 	public function edit_preferences()
@@ -359,20 +370,20 @@ class UsersController extends printController {
 			sendBack();
 		}
 		$user = $this->_uses[$this->modeltype];
-		
+
 		// Cater for no module to edit.
-		if (empty($this->_data['for_module'])) 
+		if (empty($this->_data['for_module']))
 		{
 			$this->_data['for_module'] = 'shared';
 		}
 
 		$classname	= ucfirst($this->_data['for_module']).'Preferences';
 		$module		= new $classname(true, 'ManagedUserPreferences', $user->username);
-		
+
 		$this->view->set('templateCode', $module->generateTemplate());
-		
+
 		$this->view->set('username', $this->_data['username']);
-		
+
 		if (!is_null($user->person))
 		{
 			$name = $user->person;
@@ -381,11 +392,11 @@ class UsersController extends printController {
 		{
 			$name = $user->username;
 		}
-		
+
 		$this->view->set('page_title', $this->getPageName($name, 'Edit '.$this->_data['for_module'].' Preferences for '));
-		
+
 	}
-	
+
 	public function save_preferences()
 	{
 
@@ -394,25 +405,25 @@ class UsersController extends printController {
 			$this->dataError();
 			sendBack();
 		}
-		
+
 		$user = $this->_uses[$this->modeltype];
-		
+
 		$classname	= ucfirst($this->_data['__moduleName']).'Preferences';
 		$module		= new $classname(true, 'ManagedUserPreferences', $user->username);
-		
+
 		$preferenceNames = $module->getPreferenceNames();
-		
+
 		$flash=Flash::Instance();
-		
+
 		$userPreferences = ManagedUserPreferences::instance($username);
-		
+
 		// FIXME: Validate incomming data against supplied values
 		foreach($preferenceNames as $preferenceName)
 		{
 			if (isset($this->_data[$preferenceName]))
 			{
 				$pref = $module->getPreference($preferenceName);
-				
+
 				if(isset($pref['type'])&&$pref['type']=='numeric')
 				{
 					if(!is_numeric($this->_data[$preferenceName]))
@@ -421,7 +432,7 @@ class UsersController extends printController {
 						continue;
 					}
 				}
-				
+
 				$userPreferences->setPreferenceValue(
 					$preferenceName,
 					$this->_data['__moduleName'],
@@ -431,7 +442,7 @@ class UsersController extends printController {
 			else
 			{
 				$preference = $module->getPreference($preferenceName);
-				
+
 				switch ($preference['type'])
 				{
 					case 'checkbox':
@@ -452,7 +463,7 @@ class UsersController extends printController {
 			}
 		}
 		$handled = $module->getHandledPreferences();
-		
+
 		foreach($handled as $name=>$preference)
 		{
 			if(!empty($this->_data[$name])&&isset($preference['callback']))
@@ -462,9 +473,9 @@ class UsersController extends printController {
 			}
 		}
 
-		
+
 		// Do stuff.
-		
+
 		if (!is_null($user->person))
 		{
 			$name = $user->person;
@@ -473,25 +484,25 @@ class UsersController extends printController {
 		{
 			$name = $user->username;
 		}
-		
+
 		$flash->addMessage( prettify($this->_data['__moduleName']) .' preferences for '.$name.' saved successfully');
 		sendTo($this->name, 'index', $this->_modules);
 
 	}
-	
+
 	private function addSidebar($username)
 	{
-		
+
 		$sidebar = new SidebarController($this->view);
-		
+
 		$sidebar->addList(
 			'Actions',
 			array(
 				'all'=>array(
 					'link'=>array('module'=>'admin','controller'=>'users','action'=>'index'),
 					'tag'=>'View all users'
-				),				
-				'spacer',		
+				),
+				'spacer',
 				'edit'=>array(
 					'link'=>array('module'=>'admin','controller'=>'users','action'=>'edit','username'=>$username),
 					'tag'=>'Edit details for '.$username
@@ -500,48 +511,48 @@ class UsersController extends printController {
 					'link'=>array('module'=>'admin','controller'=>'users','action'=>'edit_preferences','username'=>$username),
 					'tag'=>'Edit preferences for '.$username
 				),
-				'spacer',		
+				'spacer',
 				'delete'=>array(
 					'link'=>array('module'=>'admin','controller'=>'users','action'=>'delete','username'=>$username),
 					'tag'=>'delete '.$username
-				)				
+				)
 			)
 		);
-		
+
 		$this->view->register('sidebar', $sidebar);
 		$this->view->set('sidebar', $sidebar);
-		
+
 	}
 
 	public function view()
 	{
-		
+
 		if (isset($this->_data['username']))
 		{
-			
+
 			$username	= $this->_data['username'];
 			$res		= $this->_uses[$this->modeltype]->load($username);
-			
+
 			if ($res === FALSE)
 			{
 				sendBack();
 			}
-			
+
 		}
 		else
 		{
 			$this->dataError();
 			sendBack();
 		}
-		
+
 		$this->view->set('companies', $res->getCompanies());
 		$this->view->set('roles', $res->getCompanyRoles());
-		
+
 		// Get preferences based on username
 		$prefs = UserPreferences::getPreferencesClass($username);
-		
+
 		$uzlets = $prefs->getDashboardContents($username);
-		
+
 		// Manipulate and combine the available and selected uzlets
 		// for display
 		foreach ($uzlets['available'] as $modules=>$module)
@@ -558,29 +569,29 @@ class UsersController extends printController {
 				}
 			}
 		}
-		
+
 		foreach ($uzlets['selected'] as $module=>$details)
 		{
 			foreach ($details as $selected)
 			{
 				$dashboard['available'][$module][$selected['name']] = $dashboard['current'][$module][$selected['name']] = $selected['title'];
 			}
-			
+
 			asort($dashboard['available'][$module]);
 		}
-		
+
 		ksort($dashboard['available']);
-		
+
 		$this->view->set('dashboard', $dashboard);
-		
+
 		$shared_prefs['shared']['items_per_page']		= $prefs->getPreferenceValue('items-per-page', 'shared');
 		$shared_prefs['shared']['pdf_preview']			= $prefs->getPreferenceValue('pdf-preview', 'shared');
 		$shared_prefs['shared']['pdf_browser-printing']	= $prefs->getPreferenceValue('pdf-browser-printing', 'shared');
 		$shared_prefs['shared']['default_page']			= $prefs->getPreferenceValue('default_page', 'shared');
-		
+
 		$default_printer = $prefs->getPreferenceValue('default_printer', 'shared');
 		$printers		 = $this->selectPrinters();
-		
+
 		if (isset($printers[$default_printer]))
 		{
 			$shared_prefs['shared']['default_printer'] = $printers[$default_printer];
@@ -589,13 +600,13 @@ class UsersController extends printController {
 		{
 			$shared_prefs['shared']['default_printer'] = '';
 		}
-		
+
 		$this->view->set('preferences', $shared_prefs);
-		
+
 		$this->addSidebar($username);
-		
+
 	}
-	
+
 }
 
 // end of UsersController.php
--- modules/public_pages/admin/templates/users/reset_passwords.tpl
+++ modules/public_pages/admin/templates/users/reset_passwords.tpl
@@ -7,20 +7,22 @@
 	{form controller='users' action="reset_passwords"}
 		<dl id="view_data_left">
 		<dt><label for="edit_roles">Select Users</label>:</dt>
-		<select multiple="multiple" name="users[]">
+		<select multiple="multiple" name="users[]" size=15>
 			{foreach from=$users item=user}
 				<option label="{$user}" value="{$user}">{$user}</option>
 			{/foreach}
 		</select>
 		</dl>
 		<dl id="view_data_right">
-		<p>If you enter a password below, all users selected to the left will have their
-		password replaced with that. If not, all users selected to the left will be randomly
-		assigned, and emailed, a new password.</p>
-		{input type="text" attribute="password" label="Password"}
+		<p class="help-text">If a password is entered below, the selected users will have their
+		password replaced. If left blank, the selected users will be assigned a randomly
+		generated password. Users will be notified via email.</p>
+		{input id="User_password" type="password" attribute="password" label="New Password"}
 		</dl>
+		<dl id="view_data_right"><span id="char-count" class="help-text"></span></dl>
 		{*fix IE7*}
 		<br /><br />
 		{submit another="false"}
 	{/form}
 {/content_wrapper}
+<script>
