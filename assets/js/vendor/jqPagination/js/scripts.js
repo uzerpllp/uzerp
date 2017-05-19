886ecce6ef0ee80f6510905235aa46e048a4708e
Steve Blamey <blameys@blueloop.net>
2017-05-19 10:53:00 +0100

    Add missing js/vendor directory

---
+++ assets/js/vendor/jqPagination/js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+	$('.pagination').jqPagination({
+		link_string	: '/?page={page_number}',
+		max_page	: 40,
+		paged		: function(page) {
+			$('.log').prepend('<li>Requested page ' + page + '</li>');
+		}
+	});
+
+});
