a1f43e3a5bb6c9ee138aa414266e772a0832cb6b
Steve Blamey <blameys@blueloop.net>
2017-08-02 13:29:52 +0100

    Set CSRF token smarty variable for uzLETs

--- lib/eglets/SimpleRenderer.php
+++ lib/eglets/SimpleRenderer.php
@@ -1,68 +1,71 @@
 <?php
 
-/** 
- *	(c) 2017 uzERP LLP (support#uzerp.com). All rights reserved. 
- * 
- *	Released under GPLv3 license; see LICENSE. 
+/**
+ *	(c) 2017 uzERP LLP (support#uzerp.com). All rights reserved.
+ *
+ *	Released under GPLv3 license; see LICENSE.
  **/
 
 class SimpleRenderer implements Renderer
 {
 
 	protected $version = '$Revision: 1.6 $';
-	
+
 	public function render(EGlet &$eglet, &$smarty)
 	{
-		
+
 		if ($eglet->should_render)
 		{
 			// clear all previous smarty vars
 			// just to stop 'cross contamination' of vars
-			
+
 			$self	= $smarty->getTemplateVars('self');
-			
+
 			$action	= $smarty->getTemplateVars('action');
-			
+
 			$module = $smarty->getTemplateVars('module');
-			
+
 			$controller = $smarty->getTemplateVars('controller');
-			
+
+			$csrf_token = $smarty->getTemplateVars('csrf_token');
+
 			$smarty->clearAllAssign();
-			
+
 			$vars = $eglet->get_vars();
-			
+
 			// loop through the vars array and assign them in smarty
 			if (!empty($vars))
 			{
-				
+
 				foreach ($vars as $key => $var)
 				{
 					$smarty->assign($key, $var);
 				}
-				
+
 			}
-			
+
 			$smarty->assign('content', $eglet->getContents());
 			$smarty->assign('self', $self);
 			$smarty->assign('action', $action);
-			
+			$smarty->assign('csrf_token', $csrf_token);
+
 			if (!isset($vars['module']))
 			{
 				$smarty->assign('module', $module);
 			}
-			
+
 			if (!isset($vars['controller']))
 			{
 				$smarty->assign('controller', $controller);
 			}
-			
+
 			$smarty->cache_lifetime = 200;
-			
+
 			echo $smarty->fetch($eglet->getTemplate());
 		}
-		
+
 	}
-	
+
 }
 
 // end of SimpleRenderer
