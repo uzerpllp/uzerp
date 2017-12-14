dcfb0015579a2fc315443ea699af73bf47e956de
Steve Blamey <blameys@blueloop.net>
2017-12-14 15:00:47 +0000

    Change search to contains

--- modules/public_pages/erp/costing/models/socostsSearch.php
+++ modules/public_pages/erp/costing/models/socostsSearch.php
@@ -1,40 +1,24 @@
 <?php
 
 /**
+ * Sales Order Product Cost Search
+ *
  * @author uzERP LLP and Martyn Shiner <mshiner@uzerp.com>
  * @license GPLv3 or later
  * @copyright (c) 2000-2017 uzERP LLP (support@uzerp.com). All rights reserved.
  **/
+class socostsSearch extends BaseSearch
+{
 
-class socostsSearch extends BaseSearch {
-
-    protected $version='$Revision: 1.4 $';
-
-    public static function useDefault(&$search_data=null, &$errors=array(), $defaults=null){
-
+    public static function useDefault(&$search_data = null, &$errors = array(), $defaults = null)
+    {
         $search = new socostsSearch($defaults);
 
-// Search by Product
-        $search->addSearchField(
-        'product_header_id',
-        'SO Product',
-        'select',
-        array(),
-        'basic'
-        );
+        // Search by Product
+        $search->addSearchField('soproduct', 'SO Product contains', 'contains', [], 'basic');
 
-        $soproducts=DataObjectFactory::Factory('SOProductlineHeader');
-        $cc=new ConstraintChain();
-
-        //  here we want only products where a cost is defined
-        $cc->add(new Constraint('soc_id', 'is not', 'NULL'));
-        $soproductslist = array('' => 'All');
-        $soproductslist += $soproducts->getAll($cc,false,true);
-        $search->setOptions('product_header_id', $soproductslist);
-
-        $search->setSearchData($search_data,$errors);
+        $search->setSearchData($search_data, $errors);
         return $search;
     }
-
 }
 ?>
