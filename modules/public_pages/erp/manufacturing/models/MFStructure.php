704b094fccf25fc0826c1c85838d92232b7ed857
Steve Blamey <blameys@blueloop.net>
2018-01-13 11:28:00 +0000

    Calculate total latest and standard cost of structure on model
    references #169

--- modules/public_pages/erp/manufacturing/models/MFStructure.php
+++ modules/public_pages/erp/manufacturing/models/MFStructure.php
@@ -1,16 +1,16 @@
 <?php
 
-/** 
- *	(c) 2017 uzERP LLP (support#uzerp.com). All rights reserved. 
- * 
- *	Released under GPLv3 license; see LICENSE. 
+/**
+ *	(c) 2017 uzERP LLP (support#uzerp.com). All rights reserved.
+ *
+ *	Released under GPLv3 license; see LICENSE.
  **/
 
 class MFStructure extends DataObject
 {
 
 	protected $version = '$Revision: 1.10 $';
-	
+
 	protected $defaultDisplayFields = array('line_no'
 											,'ststructure'	=>'stock item'
 											,'start_date'
@@ -22,32 +22,51 @@ class MFStructure extends DataObject
 
 	function __construct($tablename = 'mf_structures')
 	{
-		
+
 		// Register non-persistent attributes
-		
+	    $this->setAdditional('latest_cost', 'numeric');
+	    $this->setAdditional('std_cost', 'numeric');
+
 		// Contruct the object
 		parent::__construct($tablename);
 
 		// Set specific characteristics
 		$this->idField			= 'id';
 		$this->identifierField	= 'stitem_id';
-		
+
  		// Define relationships
  		$this->belongsTo('STuom', 'uom_id', 'uom');
 		$this->belongsTo('STItem', 'stitem_id', 'stitem');
-		$this->belongsTo('STItem', 'ststructure_id', 'ststructure'); 
-		$this->hasOne('STItem', 'ststructure_id', 'ststr_item'); 
-		
+		$this->belongsTo('STItem', 'ststructure_id', 'ststructure');
+		$this->hasOne('STItem', 'ststructure_id', 'ststr_item');
+
 		// Define field formats
-		
+
 		// Define Validation
 		$this->validateUniquenessOf(array('stitem_id', 'line_no', 'start_date'));
-		
+
 		// set formatters, more set in load() function
 
 		// Define enumerated types
 
 		// Define link rules for related items
+	}
+
+	function cb_loaded($success)
+	{
+	    $this->latest_cost = add(
+	        $this->latest_mat,
+	        $this->latest_lab,
+	        $this->latest_ohd,
+	        $this->latest_osc
+	        );
+
+	    $this->std_cost = add(
+	        $this->std_mat,
+	        $this->std_lab,
+	        $this->std_ohd,
+	        $this->std_osc
+	        );
 	}
 
 	public function isActive()
@@ -56,48 +75,48 @@ class MFStructure extends DataObject
 		{
 			return true;
 		}
-		
+
 		$timestamp = mktime(0, 0, 0, date('m'), date('d'), date('Y'));
-		
+
 		return (strtotime($this->end_date) >= $timestamp);
 	}
-	
+
 	public function getSubstitute()
 	{
 		$cc = new ConstraintChain;
-		
+
 		$cc->add(new Constraint('line_no', '=', $this->line_no));
 		$cc->add(new Constraint('stitem_id', '=', $this->stitem_id));
-		
+
 		$date = $this->end_date;
-		
+
 		if (!$this->end_date)
 		{
 			$date = date('Y-m-d');
 		}
-		
+
 		$cc->add(new Constraint('start_date', '>', $date));
-		
+
 		$substitute = DataObjectFactory::Factory('MFStructure');
-		
+
 		return $substitute->loadBy($cc);
 	}
 
 	public function getRequirement ($qty=1)
 	{
-		return bcmul(round($this->qty*$qty*100/(100-$this->waste_pc),$this->ststr_item->qty_decimals),1,$this->ststr_item->qty_decimals);	
+		return bcmul(round($this->qty*$qty*100/(100-$this->waste_pc),$this->ststr_item->qty_decimals),1,$this->ststr_item->qty_decimals);
 	}
-	
+
 	public static function globalRollOver()
 	{
 		$db = DB::Instance();
-		
+
 		$date = date('Y-m-d');
-		
+
 		$query = "UPDATE mf_structures
 					SET std_cost=latest_cost,std_mat=latest_mat,std_lab=latest_lab,std_osc=latest_osc,std_ohd=latest_ohd
 					WHERE (start_date <= '".$date."' OR start_date IS NULL) AND (end_date > '".$date."' OR end_date IS NULL) AND usercompanyid=".EGS_COMPANY_ID;
-		
+
 		return ($db->Execute($query) !== false);
 	}
 
@@ -109,25 +128,25 @@ class MFStructure extends DataObject
 		$success = true;
 		// Get the required action for the Item if enabled on Item Product Type
 		$stitem = DataObjectFactory::Factory('STItem');
-		
+
 		if ($stitem->load($data['stitem_id']))
 		{
 // TODO: need a check to ensure that there is only one transfer rule for action
 			$staction=$stitem->getAction('backflush');
-			
+
 			if (!is_null($staction))
 			{
 // Get the transfer rule for the action
 				$data['whaction_id']=$staction;
-				
+
 				$transrule = DataObjectFactory::Factory('WHTransferrule');
-				
+
 				$cc = new ConstraintChain();
-				
+
 				$cc->add(new Constraint('whaction_id','=', $staction));
-				
+
 				$transrule->loadBy($cc);
-				
+
 				foreach ($elements as $structure)
 				{
 // Insert transaction pair for WIP Update
@@ -136,21 +155,21 @@ class MFStructure extends DataObject
 // Currently cannot define bins for backflushing
 					$data['from_whbin_id']	= '';
 					$data['to_whbin_id']	= '';
-					
+
 // Calculate the quantity used of the sub-component
 					$data['qty']		= $data['book_qty']*$structure->qty*100/(100-$structure->waste_pc);
 					$data['stitem_id']	= $structure->ststructure_id;
 					// Status is initially OK
 					$data['status'] = 'O';
-					
+
 					$stitem = DataObjectFactory::Factory('STItem');
-					
+
 					if ($stitem->load($structure->ststructure_id))
 					{
 						$converted = $stitem->convertToUoM($structure->uom_id
 														,$stitem->uom_id
 														,$data['qty']);
-						
+
 						if ($converted === false)
 						{
 							$errors[] = 'UoM Conversion Error on item '.$stitem->item_code.' - '.$stitem->description;
@@ -159,7 +178,7 @@ class MFStructure extends DataObject
 						{
 // Validate the data
 							$data['qty'] = round($converted, $stitem->qty_decimals);
-							
+
 							$models = STTransaction::prepareMove($data, $errors);
 						}
 					}
@@ -175,9 +194,9 @@ class MFStructure extends DataObject
 					}
 					// Save transaction pair
 					$success = true;
-					
+
 					$db->StartTrans();
-					
+
 					foreach ($models as $model)
 					{
 						$ignored_errors = array();
@@ -191,40 +210,40 @@ class MFStructure extends DataObject
 						$db->FailTrans();
 						break;
 					}
-					
+
 					$db->CompleteTrans();
-					
+
 					// If transactions were saved then go to next transaction pair
 					if ($success)
 					{
 						continue;
 					}
-					
+
 					// Save transaction pair with error status
 					$success = true;
-					
+
 					$db->StartTrans();
-					
+
 					foreach ($models as $model)
 					{
 						$model->error_qty	= $model->qty;
 						$model->qty			= 0;
 						$model->status		= 'E';
-						
+
 						// If transaction was saved then go to next transaction
 						if ($model->save($errors) !== false)
 						{
 							continue;
 						}
-						
+
 						// Error saving transaction
 						$success = false;
 						$db->FailTrans();
 						break;
 					}
-					
+
 					$db->CompleteTrans();
-					
+
 					// If errors occurred saving transaction pairs then exit loop
 					if (!$success)
 					{
@@ -251,7 +270,6 @@ class MFStructure extends DataObject
 	{
 		return $this->ststr_item->currentBalance();
 	}
-
 }
 
 // End of MFStructure
