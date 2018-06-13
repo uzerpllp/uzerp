5524df3ad167bda0b025bdf917b8f3d86423b663
Steve Blamey <blameys@blueloop.net>
2018-06-13 15:59:21 +0100

    Use correct view source

--- schema/phinx/migrations/20180605155840_view_gl_taxeupurchases.php
+++ schema/phinx/migrations/20180605155840_view_gl_taxeupurchases.php
@@ -1,6 +1,5 @@
 <?php
 
-
 use UzerpPhinx\UzerpMigration;
 
 class ViewGlTaxeupurchases extends UzerpMigration
@@ -24,16 +23,24 @@ SELECT tr1.id,
     tr1.comment,
     tr1.type,
     tr1.value AS vat,
-    tr2.net,
+    CASE
+            WHEN tr1.type::text = 'C'::text THEN 0::numeric - pih.base_net_value
+            ELSE pih.base_net_value
+    END AS net,
     tr1.usercompanyid,
-    a.account
+    a.account,
+    c.name AS supplier,
+    pih.ext_reference,
+    ts.eu_tax AS eutaxstatus
     FROM gl_transactions tr1
     JOIN glparams_eu_acquisitions vi ON vi.paramvalue_id = tr1.glaccount_id
     JOIN gl_accounts a ON a.id = tr1.glaccount_id
-    JOIN gltransactions_noncontrol tr2 ON tr1.docref::text = tr2.docref::text AND tr1.source::text = tr2.source::text AND tr1.type::text = tr2.type::text
-    JOIN pi_header pih ON pih.invoice_number = tr1.docref::integer
+    JOIN pi_header pih ON tr1.docref::text = pih.invoice_number::text AND tr1.type::text = pih.transaction_type::text AND tr1.source::text = 'P'::text
+    JOIN plmaster plm ON pih.plmaster_id = plm.id
+    JOIN company c ON plm.company_id = c.id
     JOIN tax_statuses ts ON ts.id = pih.tax_status_id AND ts.eu_tax = true
-    WHERE tr1.source::text = 'P'::text AND (tr1.type::text = 'I'::text AND tr1.value > 0::numeric OR tr1.type::text = 'C'::text AND tr1.value < 0::numeric);
+    WHERE tr1.source::text = 'P'::text AND (tr1.type::text = 'I'::text AND tr1.value > 0::numeric OR tr1.type::text = 'C'::text AND tr1.value < 0::numeric)
+    order by 1;
 VIEW;
 
         $this->query("DROP VIEW {$view_name}");
