ed50e03e593c5ee8abb223dd281552f1515799b9
Steve Blamey <blameys@blueloop.net>
2018-09-06 12:04:46 +0100

    Enable rollback

--- schema/phinx/migrations/20180904121233_remove_ean.php
+++ schema/phinx/migrations/20180904121233_remove_ean.php
@@ -6,12 +6,22 @@ use UzerpPhinx\UzerpMigration;
 class RemoveEan extends UzerpMigration
 {
     /**
-     * Remove EAN field from productlines
+     * Remove EAN from sales productline
      */
-    public function change()
+    public function up()
     {
         $table = $this->table('so_product_lines');
         $table->removeColumn('ean')
+              ->save();
+    }
+
+    /**
+     * Add EAN to sales productline
+     */
+    public function down()
+    {
+        $table = $this->table('so_product_lines');
+        $table->addColumn('ean', 'text', ['null' => true])
               ->save();
     }
 }
