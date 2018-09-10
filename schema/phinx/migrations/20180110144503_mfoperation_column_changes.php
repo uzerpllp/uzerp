80d249209de0de50bca362f55e9cb5c10264bca3
Steve Blamey <steve.blamey@adbury.net>
2018-09-10 15:17:27 +0100

    Add fields to support routing outside operations

--- schema/phinx/migrations/20180110144503_mfoperation_column_changes.php
+++ schema/phinx/migrations/20180110144503_mfoperation_column_changes.php
@@ -12,7 +12,13 @@ class MfoperationColumnChanges extends AbstractMigration
     {
         $mfops_require_volume_target = $this->execute("ALTER TABLE mf_operations ALTER COLUMN volume_target SET NOT NULL;");
         $mfops = $this->table('mf_operations')
-        ->addColumn('batch_op', 'boolean', ['default' => false])
+        ->addColumn('outside_processing_cost', 'decimal', ['default' => 0, 'null'=> true])
+        ->addColumn('type', 'string', ['length' => 1, 'default' => 'R', 'null' => false])
+        ->addColumn('lead_time', 'integer', ['default' => '0', 'null' => true])
+        ->addColumn('po_productline_header_id', 'biginteger', ['null' => true])
+        ->addColumn('description', 'string', ['null' => true])
+        ->addColumn('latest_osc', 'decimal', ['default' => '0'])
+        ->addColumn('std_osc', 'decimal', ['default' => '0'])
         ->save();
     }
 
@@ -22,7 +28,13 @@ class MfoperationColumnChanges extends AbstractMigration
     public function down() {
         $mfops_do_not_require_volume_target = $this->execute("ALTER TABLE mf_operations ALTER COLUMN volume_target DROP NOT NULL;");
         $mfops = $this->table('mf_operations')
-        ->removeColumn('batch_op')
+        ->removeColumn('outside_processing_cost')
+        ->removeColumn('type')
+        ->removeColumn('lead_time')
+        ->removeColumn('po_productline_header_id')
+        ->removeColumn('description')
+        ->removeColumn('latest_osc')
+        ->removeColumn('std_osc')
         ->save();
     }
 }
--- schema/phinx/migrations/20180611104830_mfoperation_overview_column_changes.php
+++ schema/phinx/migrations/20180611104830_mfoperation_overview_column_changes.php
@@ -19,6 +19,7 @@ SELECT o.id,
     o.start_date,
     o.end_date,
     o.remarks,
+    o.description,
     o.stitem_id,
     o.mfcentre_id,
     o.mfresource_id,
@@ -29,7 +30,62 @@ SELECT o.id,
     o.uptime_target,
     o.volume_target,
     o.resource_qty,
-    o.batch_op,
+    o.lead_time,
+    o.type,
+    o.std_cost,
+    o.std_lab,
+    o.std_ohd,
+    o.std_osc,
+    o.latest_cost,
+    o.latest_lab,
+    o.latest_ohd,
+    o.latest_osc,
+    o.created,
+    o.createdby,
+    o.alteredby,
+    o.lastupdated,
+    (s.item_code::text || ' - '::text) || s.description::text AS stitem,
+    s.obsolete_date,
+    u.uom_name AS volume_uom,
+    c.centre,
+    r.description AS resource,
+    o.po_productline_header_id,
+    ppl.description as product_description,
+    o.outside_processing_cost
+    FROM mf_operations o
+    JOIN st_items s ON o.stitem_id = s.id
+    JOIN st_uoms u ON o.volume_uom_id = u.id
+    JOIN mf_centres c ON o.mfcentre_id = c.id
+    JOIN mf_resources r ON o.mfresource_id = r.id
+    LEFT JOIN po_product_lines_header ppl ON ppl.id = o.po_productline_header_id;
+VIEW;
+
+        $this->query("DROP VIEW {$view_name}");
+        $this->query($view);
+        $this->query("ALTER TABLE {$view_name} OWNER TO \"{$view_owner}\"");
+    }
+
+    public function down()
+    {
+        $view_name = 'mf_operationsoverview';
+        $view_owner = 'www-data';
+        $view = <<<'VIEW'
+CREATE OR REPLACE VIEW mf_operationsoverview AS 
+SELECT o.id,
+    o.op_no,
+    o.start_date,
+    o.end_date,
+    o.remarks,
+    o.stitem_id,
+    o.mfcentre_id,
+    o.mfresource_id,
+    o.usercompanyid,
+    o.volume_period,
+    o.volume_uom_id,
+    o.quality_target,
+    o.uptime_target,
+    o.volume_target,
+    o.resource_qty,
     o.std_cost,
     o.std_lab,
     o.std_ohd,
