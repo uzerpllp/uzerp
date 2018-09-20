f021e2df4503f3d1f3832866a6152c7854a34448
Steve Blamey <steve.blamey@adbury.net>
2018-09-20 15:23:38 +0100

    Remove overview migration

--- schema/phinx/migrations/20180830095909_frepple_input_tables.php
+++ schema/phinx/migrations/20180830095909_frepple_input_tables.php
@@ -3,6 +3,9 @@
 
 use UzerpPhinx\UzerpMigration;
 
+/**
+ * Database tables to hold planned orders
+ */
 class FreppleInputTables extends UzerpMigration
 {
     protected $owner = 'www-data';
@@ -10,44 +13,23 @@ class FreppleInputTables extends UzerpMigration
     public function change()
     {
         $po_planned = $this->table('po_planned');
-        $po_planned->addColumn('item_code', 'string')
-                   ->addColumn('quantity', 'integer')
-                   ->addColumn('stitem_id', 'biginteger')
-                   ->addColumn('plmaster_id', 'biginteger')
-                   ->addColumn('startdate', 'datetime')
-                   ->addColumn('enddate', 'datetime')
+        $po_planned->addColumn('item_code', 'string', ['null' => true])
+                   ->addColumn('quantity', 'integer', ['null' => true])
+                   ->addColumn('stitem_id', 'biginteger', ['null' => true])
+                   ->addColumn('plmaster_id', 'biginteger', ['null' => true])
+                   ->addColumn('startdate', 'datetime', ['null' => true])
+                   ->addColumn('enddate', 'datetime', ['null' => true])
                    ->save();
 
         $mf_planned = $this->table('mf_planned');
-        $mf_planned->addColumn('item_code', 'string')
-                   ->addColumn('quantity', 'integer')
-                   ->addColumn('stitem_id', 'biginteger')
-                   ->addColumn('startdate', 'datetime')
-                   ->addColumn('enddate', 'datetime')
+        $mf_planned->addColumn('item_code', 'string', ['null' => true])
+                   ->addColumn('quantity', 'integer', ['null' => true])
+                   ->addColumn('stitem_id', 'biginteger', ['null' => true])
+                   ->addColumn('startdate', 'datetime', ['null' => true])
+                   ->addColumn('enddate', 'datetime', ['null' => true])
                    ->save();
 
         $this->query("ALTER TABLE po_planned OWNER TO \"{$this->owner}\"");
         $this->query("ALTER TABLE mf_planned OWNER TO \"{$this->owner}\"");
-    }
-
-    public function up()
-    {
-        $view_name = 'mf_plannedoverview';
-        $view = <<<VIEW
- SELECT sti.id AS stitem_id,
-    sti.item_code,
-    sti.description,
-    mfp.quantity AS qty,
-    sti.uom_id,
-    u.uom_name,
-    mfp.startdate AS start_date,
-    mfp.enddate AS required_by
-   FROM mf_planned mfp
-     JOIN st_items sti ON sti.id = mfp.stitem_id
-     JOIN st_uoms u ON u.id = sti.uom_id;
-VIEW;
-
-        $this->query($view);
-        $this->query("ALTER TABLE {$view_name} OWNER TO \"{$this->owner}\"");
     }
 }
