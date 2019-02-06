d767ef82768a5bf2bf06dd2ba0e619523863e452
Steve Blamey <sblamey@uzerp.com>
2019-02-06 15:57:36 +0000

    Update copyrights

--- modules/public_pages/erp/ledger/vat/models/VatEuSalesCollection.php
+++ modules/public_pages/erp/ledger/vat/models/VatEuSalesCollection.php
@@ -1,7 +1,14 @@
 <?php
-
-
-
+/**
+ *	@author uzERP LLP and Steve Blamey <sblamey@uzerp.com>
+ *	@license GPLv3 or later
+ *	@copyright (c) 2019 uzERP LLP (support#uzerp.com). All rights reserved.
+ *
+ *	uzERP is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	any later version.
+ */
 class VatEuSalesCollection extends DataObjectCollection {
     
     public $field;
--- modules/public_pages/erp/ledger/vat/models/VatOutputs.php
+++ modules/public_pages/erp/ledger/vat/models/VatOutputs.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ *	@author uzERP LLP and Steve Blamey <sblamey@uzerp.com>
+ *	@license GPLv3 or later
+ *	@copyright (c) 2019 uzERP LLP (support#uzerp.com). All rights reserved.
+ *
+ *	uzERP is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	any later version.
+ */
 class VatOutputs extends DataObject {
     protected $defaultDisplayFields = [
         'transaction_date',
