52750dffe7e5f87eb6c33ea2e1eb6eb666a7c60d
Steve Blamey <sblamey@uzerp.com>
2019-04-18 14:51:50 +0100

    Update copyright header

--- schema/phinx/migrations/20190417152216_vat_return_column_spelling_fix.php
+++ schema/phinx/migrations/20190417152216_vat_return_column_spelling_fix.php
@@ -2,6 +2,13 @@
 
 use UzerpPhinx\UzerpMigration;
 
+/**
+ * Phinx Migration
+ *
+ * @author uzERP LLP and Steve Blamey <sblamey@uzerp.com>
+ * @license GPLv3 or later
+ * @copyright (c) 2019 uzERP LLP (support#uzerp.com). All rights reserved.
+ */
 class VatReturnColumnSpellingFix extends UzerpMigration
 {
     public function change()
--- schema/phinx/migrations/20190417154143_vat_return_overview_spelling_fix.php
+++ schema/phinx/migrations/20190417154143_vat_return_overview_spelling_fix.php
@@ -1,8 +1,14 @@
 <?php
 
-
 use UzerpPhinx\UzerpMigration;
 
+/**
+ * Phinx Migration
+ *
+ * @author uzERP LLP and Steve Blamey <sblamey@uzerp.com>
+ * @license GPLv3 or later
+ * @copyright (c) 2019 uzERP LLP (support#uzerp.com). All rights reserved.
+ */
 class VatReturnOverviewSpellingFix extends UzerpMigration
 {
     private $view_name = 'vat_return_overview';
