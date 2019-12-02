94df094849386b3920417c12595f3b43e5dbf632
Steve Blamey <sblamey@uzerp.com>
2019-12-02 11:57:26 +0000

    Change assignment operator to comparison when overriding transaction type
    closes #201

--- modules/public_pages/erp/ledger/general_ledger/controllers/GltransactionsController.php
+++ modules/public_pages/erp/ledger/general_ledger/controllers/GltransactionsController.php
@@ -303,7 +303,7 @@ class GltransactionsController extends printController
 		
 		$this->_data[$transaction_model]['source']	= 'G';
 		$this->_data[$transaction_model]['type']	= 'J';
-		if ($glheader->type = 'Y') {
+		if ($glheader->type == 'Y') {
 			$this->_data[$transaction_model]['type']	= 'Y';
 		}
 		
