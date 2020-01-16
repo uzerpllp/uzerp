1806abba2434f53e2424d7f0c33d30334baf739c
Steve Blamey <sblamey@uzerp.com>
2020-01-16 10:51:50 +0000

    Update copyright year

--- modules/public_pages/login/controllers/IndexController.php
+++ modules/public_pages/login/controllers/IndexController.php
@@ -10,7 +10,7 @@ use PHPMailer\PHPMailer\Exception;
  * @package login
  * @author uzERP LLP and Steve Blamey <blameys@blueloop.net>
  * @license GPLv3 or later
- * @copyright (c) 2017 uzERP LLP (support#uzerp.com). All rights reserved.
+ * @copyright (c) 2020 uzERP LLP (support#uzerp.com). All rights reserved.
  **/
 class IndexController extends Controller
 {
