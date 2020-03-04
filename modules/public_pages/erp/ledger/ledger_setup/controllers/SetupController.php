ae3c84107c2f5adc8657bf9767ac52233d50fa87
Steve Blamey <sblamey@uzerp.com>
2020-03-04 11:10:21 +0000

    Ledger setup, add module preference for invoice layout report type

--- modules/public_pages/erp/ledger/ledger_setup/controllers/SetupController.php
+++ modules/public_pages/erp/ledger/ledger_setup/controllers/SetupController.php
@@ -16,7 +16,34 @@ class SetupController extends MasterSetupController
 			'sy_delivery_terms'=>'DeliveryTerm',
 			'intrastat_trans_types'=>'IntrastatTransType'
 		);
-		
+	
+	protected $setup_preferences = [
+		'sales-invoice-report-type' => 'Report type to be used for Sales Invoice print layout options',
+	];
+
+	protected function registerPreference()
+	{
+		parent::registerPreference();
+
+		$salesInvoiceReportType = $this->module_preferences['sales-invoice-report-type']['preference'];
+
+		$reportTypes = new ReportType();
+		$types = $reportTypes->getPrivateReportTypes();
+		$data = [];
+		$data[] = ['label' => 'None', 'value' => ''];
+        foreach ($types as $key => $item) {
+            $data[] = ['label' => $item, 'value' => $key];
+        }
+
+		$this->preferences->registerPreference(array(
+			'name' => 'sales-invoice-report-type',
+			'display_name' => $this->module_preferences['sales-invoice-report-type']['title'],
+			'type' => 'select',
+			'data' => $data,
+			'value' => $salesInvoiceReportType,
+			'default' => ''
+		));
+	}
 }
 
 // End of SetupController
