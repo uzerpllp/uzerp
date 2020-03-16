d382aafb167615090d17b2b2e5eccc42382760ee
Steve Blamey <sblamey@uzerp.com>
2020-03-16 15:16:01 +0000

    Migration: use select distinct in addressoverview to prevent duplicate entries
    Duplicate addresses were causing duplicate rows to be returned in other views
    that depend on addressoverview. This in turn caused issues with paging on associated
    uzERP list views.

---
+++ schema/phinx/migrations/20200316151116_address_overview_distinct.php
@@ -0,0 +1,78 @@
+<?php
+
+
+use UzerpPhinx\UzerpMigration;
+
+/**
+ * Use select distinct to avoid duplicate entries
+ */
+class AddressOverviewDistinct extends UzerpMigration
+{
+    public function up()
+    {
+        $view_name = 'addressoverview';
+        $view_owner = 'www-data';
+        $view = <<<'VIEW'
+CREATE OR REPLACE VIEW public.addressoverview
+AS
+SELECT DISTINCT a.id,
+    a.street1,
+    a.street2,
+    a.street3,
+    a.town,
+    a.county,
+    a.postcode,
+    a.countrycode,
+    a.usercompanyid,
+    a.created,
+    a.createdby,
+    a.alteredby,
+    a.lastupdated,
+    co.name AS country,
+    pa.vatnumber,
+    (((((((((((a.street1::text || ', '::text) || COALESCE(a.street2, ''::character varying)::text) || ', '::text) || COALESCE(a.street3, ''::character varying)::text) || ', '::text) || a.town::text) || ', '::text) || COALESCE(a.county, ''::character varying)::text) || ', '::text) || COALESCE(a.postcode, ''::character varying)::text) || ', '::text) || co.name::text AS address
+    FROM address a
+    JOIN partyaddress pa ON pa.address_id = a.id
+    JOIN countries co ON a.countrycode = co.code;
+VIEW;
+        $this->query("select deps_save_and_drop_dependencies('public', '{$view_name}')");
+        $this->query("DROP VIEW {$view_name}");
+        $this->query($view);
+        $this->query("ALTER TABLE {$view_name} OWNER TO \"{$view_owner}\"");
+        $this->query("select deps_restore_dependencies('public', '{$view_name}')");
+    }
+
+    public function down()
+    {
+        $view_name = 'addressoverview';
+        $view_owner = 'www-data';
+        $view = <<<'VIEW'
+CREATE OR REPLACE VIEW public.addressoverview
+AS
+SELECT a.id,
+    a.street1,
+    a.street2,
+    a.street3,
+    a.town,
+    a.county,
+    a.postcode,
+    a.countrycode,
+    a.usercompanyid,
+    a.created,
+    a.createdby,
+    a.alteredby,
+    a.lastupdated,
+    co.name AS country,
+    pa.vatnumber,
+    (((((((((((a.street1::text || ', '::text) || COALESCE(a.street2, ''::character varying)::text) || ', '::text) || COALESCE(a.street3, ''::character varying)::text) || ', '::text) || a.town::text) || ', '::text) || COALESCE(a.county, ''::character varying)::text) || ', '::text) || COALESCE(a.postcode, ''::character varying)::text) || ', '::text) || co.name::text AS address
+    FROM address a
+    JOIN partyaddress pa ON pa.address_id = a.id
+    JOIN countries co ON a.countrycode = co.code;
+VIEW;
+        $this->query("select deps_save_and_drop_dependencies('public', '{$view_name}')");
+        $this->query("DROP VIEW {$view_name}");
+        $this->query($view);
+        $this->query("ALTER TABLE {$view_name} OWNER TO \"{$view_owner}\"");
+        $this->query("select deps_restore_dependencies('public', '{$view_name}')");
+    }
+}
