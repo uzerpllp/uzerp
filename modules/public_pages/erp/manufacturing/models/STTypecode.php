48910ba3ea50ef64e28832ad75be5f2655781170
Steve Blamey <sblamey@uzerp.com>
2020-06-17 09:58:50 +0100

    STTypecode model: add relationship for return action and adjust overview

--- modules/public_pages/erp/manufacturing/models/STTypecode.php
+++ modules/public_pages/erp/manufacturing/models/STTypecode.php
@@ -12,15 +12,17 @@ class STTypecode extends DataObject
 	protected $version='$Revision: 1.6 $';
 	
 	protected $defaultDisplayFields = array('type_code'
-											,'description',
-											'active',
-											'comp_class'
+											,'description'
+											,'active'
+											,'comp_class'
 											,'backflush_action'
 											,'complete_action'
 											,'issue_action'
+											,'return_action'
 											,'backflush_action_id'
 											,'complete_action_id'
 											,'issue_action_id'
+											,'return_action_id'
 											);
 	
 	function __construct($tablename='st_typecodes')
@@ -36,6 +38,7 @@ class STTypecode extends DataObject
 		$this->belongsTo('WHAction', 'backflush_action_id', 'backflush_action');
 		$this->belongsTo('WHAction', 'complete_action_id', 'complete_action');
 		$this->belongsTo('WHAction', 'issue_action_id', 'issue_action');
+		$this->belongsTo('WHAction', 'return_action_id', 'return_action');
 		
 		$stitem = new STItem();
 		$this->setEnum('comp_class', $stitem->getEnumOptions('comp_class',1));
