546f07fa9f09006ce30dcbfa9bcc8deae87144a3
Steve Blamey <sblamey@uzerp.com>
2022-05-11 14:57:51 +0100

    Database migration, remove OIDS from tables that have them defined.

---
+++ schema/phinx/migrations/20220511121240_remove_oids.php
@@ -0,0 +1,36 @@
+<?php
+
+
+use UzerpPhinx\UzerpMigration;
+
+/**
+ * Remove unnecessary OIDS from tables
+ * 
+ */
+class RemoveOids extends UzerpMigration
+{
+    private $tables = [
+        'public.activities',
+        'public.activitytype',
+        'public.campaigns',
+        'public.campaignstatus',
+        'public.campaigntype',
+        'public.companies_in_categories',
+        'public.company',
+        'public.contact_categories',
+        'public.countries',
+        'public.lang',
+        'public.ledger_categories',
+        'public.opportunities',
+        'public.opportunitysource',
+        'public.opportunitystatus',
+        'public.users'
+    ];
+
+    public function up()
+    {
+        foreach ($this->tables as $table) {
+            $this->query("ALTER TABLE {$table} SET WITHOUT OIDS");
+        }
+    }
+}
