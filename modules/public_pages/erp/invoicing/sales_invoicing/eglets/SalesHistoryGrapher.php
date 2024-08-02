d2f13077ceb78801f184156ff18911335face082
Steve Blamey <sblamey@uzerp.com>
2024-08-02 11:09:31 +0100

    fix(uzlets): PHP8.3 fixes

--- modules/public_pages/erp/invoicing/sales_invoicing/eglets/SalesHistoryGrapher.php
+++ modules/public_pages/erp/invoicing/sales_invoicing/eglets/SalesHistoryGrapher.php
@@ -1,5 +1,5 @@
 <?php
- 
+
 /** 
  *	(c) 2017 uzERP LLP (support#uzerp.com). All rights reserved. 
  * 
@@ -9,43 +9,43 @@
 class SalesHistoryGrapher extends SimpleGraphEGlet {
 
 	protected $version='$Revision: 1.9 $';
-	
+
 	protected $template = 'sorders_overview.tpl';
-	
+
 	function populate() {
-		
+
 		$orders				= new SInvoiceCollection(new SInvoice);
 		$customersales 		= $orders->getSalesHistory();
-		
+
 		$db					= &DB::Instance();
 		$param				= new GLParams();
-		$currency_symbol	= utf8_decode($param->base_currency_symbol());
+		$currency_symbol	= (string) $param->base_currency_symbol();
 		$options			= array();
-					
+
 		$options['header']['text']  = 'Sales this month '.$param->base_currency_symbol().$customersales['current']['this_month_to_date']['value'];
 		$options['header']['text'] .= ' : this week '.$param->base_currency_symbol().$customersales['current']['this_week']['value'];
 		$options['header']['text'] .= ' : last week '.$param->base_currency_symbol().$customersales['current']['last_week']['value'];
-		
+
 		$options['header']['textStyle']['font-size'] = '14pt';
-		
+
 		ksort($customersales['previous']);
-		
+
 		$options['date_axis'] = TRUE;
-		
+
 		$sales_counter = 0;
-		
+
 		// build up data array
 		foreach ($customersales['previous'] as $period => $value) {
-			
+
 			$date = explode('/', $period);
-			
+
 			$data['x'][] = $date[1] . "/1/" . $date[0];
 			$data['y'][] = (float) $value['value'];
-			
+
 			$label[0][$sales_counter++] = number_format($value['value'], 2);
-			
+
 		}
-		
+
 		$options['seriesList'][] = array(
 			'legendEntry'	=> FALSE,
 			'data' 			=> $data,
@@ -55,15 +55,15 @@ class SalesHistoryGrapher extends SimpleGraphEGlet {
 				'type'		=> 'circle'
 			)
 		);
-		
+
 		$options['type']		= 'line';
 		$options['identifier']	= __CLASS__;
 		$options['labels'] 		= $label;
-		
+
 		$this->contents = json_encode($options);
-		
+
 	}
-	
+
 }
 
 // end of SalesHistoryGrapher.php
