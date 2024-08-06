8a94e326d8855a71b8436c10b7b9b111f810fcc8
Steve Blamey <sblamey@uzerp.com>
2024-08-06 11:05:50 +0100

    chore(rector): update config

--- rector.php
+++ rector.php
@@ -10,25 +10,30 @@ return RectorConfig::configure()
 
     ->withPaths([
         __DIR__ . '/lib',
-        __DIR__ . '/lib/classes/standard',
         __DIR__ . '/modules',
         __DIR__ . '/plugins',
     ])
 
+
+    ->withIndent(indentChar: '	', indentSize: 1)
+
     //define sets of rules
-    //->withPhpSets()
+    ->withPhpSets()
 
     ->withPhpVersion(PhpVersion::PHP_83)
 
-    //->withSets([
-//	LevelSetList::UP_TO_PHP_83
-  //  ])
-
-    ->withRules([
-	Rector\Php80\Rector\ClassMethod\AddParamBasedOnParentClassMethodRector::class
+    ->withSets([
+	LevelSetList::UP_TO_PHP_83
     ])
 
+//    ->withRules([
+//	Rector\Php70\Rector\Ternary\TernaryToNullCoalescingRector::class,
+//	Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector::class,
+//	Rector\Transform\Rector\Class_\AddAllowDynamicPropertiesAttributeRector::class
+//    ])
+
     ->withSkip([
+	Rector\Php80\Rector\ClassMethod\AddParamBasedOnParentClassMethodRector::class,
 	// this is fixed in code formatting before applying rector
         Rector\Php54\Rector\Array_\LongArrayToShortArrayRector::class,
 	// drop this for php 7.4+
