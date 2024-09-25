9a508b13a8d655d60a09cc5116917c97a73a3c91
Steve Blamey <sblamey@uzerp.com>
2024-09-25 15:11:36 +0100

    fix(CRM): show value instead of cost in Opportunity Source By Value uzLET

--- modules/public_pages/crm/eglets/OpportunitiesBySourceGrapher.php
+++ modules/public_pages/crm/eglets/OpportunitiesBySourceGrapher.php
@@ -12,9 +12,9 @@ class OpportunitiesBySourceGrapher extends SimpleGraphEGlet {
 	function populate() {
 		$db	= &DB::Instance();
 
-		$query = $db->prepare("select s.name, COALESCE(sum(o.cost), 0.0) AS piplinecost FROM opportunitysource s LEFT OUTER JOIN opportunities o ON (s.id=o.source_id
+		$query = $db->prepare("select s.name, COALESCE(sum(o.value), 0.0) AS piplinevalue FROM opportunitysource s LEFT OUTER JOIN opportunities o ON (s.id=o.source_id
 		AND o.usercompanyid=? AND extract('month' FROM o.enddate)=extract('month' FROM now())
-		AND extract('year' FROM o.enddate)=extract('year' FROM now())) WHERE s.usercompanyid=? GROUP BY s.name ORDER BY COALESCE(sum(o.cost), 0.0), s.name");
+		AND extract('year' FROM o.enddate)=extract('year' FROM now())) WHERE s.usercompanyid=? GROUP BY s.name ORDER BY COALESCE(sum(o.value), 0.0), s.name");
 
 		$vars = [EGS_COMPANY_ID, EGS_COMPANY_ID];
 		$result		= $db->GetAssoc($query, $vars);
