dea7e765a2ad089808b81c1f0f9eb3b52dc58bfa
Steve Blamey <sblamey@uzerp.com>
2025-07-16 11:01:43 +0100

    refactor(RouteParser): Make singleton instance resettable
    Refactors the RouteParser singleton to use a static property for its
    instance. This change enables the addition of a public resetInstance()
    method, allowing the test suite to clear the singleton's state and
    prevent state leakage between tests.

--- lib/classes/standard/RouteParser.php
+++ lib/classes/standard/RouteParser.php
@@ -11,19 +11,29 @@ class RouteParser {
 	
 	protected $routes = array();
 	protected $dispatch = array();
+	private static $instance;
 	
 	private function __construct() {
 		$this->dispatch=$_GET;	
 	}
 	
 	public static function Instance() {
-		static $instance;
 		
-		if (!isset($instance)) {
-			$instance = new RouteParser();
+		if (!isset(self::$instance)) {
+			self::$instance = new RouteParser();
 		}
 		
-		return $instance;
+		return self::$instance;
+	}
+
+	/**
+	 * Resets the singleton instance.
+	 * Used for testing.
+	 *
+	 * @return void
+	 */
+	public static function resetInstance() {
+		self::$instance = null;
 	}
 	
 	public function AddRoute ($route) {
